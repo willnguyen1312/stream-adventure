9a49844d4b503113f792a03a3abf1950ec970cbb
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-03-15 16:19:20 +0700

    17

---
+++ 17/program.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto')
+const tar = require('tar')
+const concat = require('concat-stream')
+
+const parser = new tar.Parse()
+parser.on('entry', function (e) {
+  if (e.type !== 'File') return e.resume()
+
+  const h = crypto.createHash('md5', { encoding: 'hex' })
+  e.pipe(h).pipe(concat(function (hash) {
+    console.log(hash + ' ' + e.path)
+  }))
+})
+
+const cipher = process.argv[2]
+const key = process.argv[3]
+const iv = process.argv[4]
+process.stdin
+  .pipe(crypto.createDecipheriv(cipher, key, iv))
+  .pipe(parser)
