8e8169aac2c256ef11f27795f05cac5a534c1dde
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-03-15 15:40:37 +0700

    Nine

---
+++ 9/program.js
@@ -0,0 +1,14 @@
+
+const http = require('http')
+const through = require('through2')
+
+const server = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    req.pipe(through(function (buf, _, next) {
+      this.push(buf.toString().toUpperCase())
+      next()
+    })).pipe(res)
+  } else res.end('send me a POST\n')
+})
+
+server.listen(parseInt(process.argv[2]))
