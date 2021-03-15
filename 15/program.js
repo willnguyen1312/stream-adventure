a76fb903b63167eb2aac1f1a02adae15990060a3
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-03-15 16:17:24 +0700

    15

---
+++ 15/program.js
@@ -0,0 +1,32 @@
+const combine = require('stream-combiner')
+const through = require('through2')
+const split2 = require('split2')
+const zlib = require('zlib')
+
+module.exports = function () {
+  const grouper = through(write, end)
+  let current
+
+  function write (line, _, next) {
+    if (line.length === 0) return next()
+    const row = JSON.parse(line)
+
+    if (row.type === 'genre') {
+      if (current) {
+        this.push(JSON.stringify(current) + '\n')
+      }
+      current = { name: row.name, books: [] }
+    } else if (row.type === 'book') {
+      current.books.push(row.name)
+    }
+    next()
+  }
+  function end (next) {
+    if (current) {
+      this.push(JSON.stringify(current) + '\n')
+    }
+    next()
+  }
+
+  return combine(split2(), grouper, zlib.createGzip())
+}
