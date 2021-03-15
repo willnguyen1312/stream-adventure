59cc6106b34e74b972374a2fe7a93884d63f491c
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-03-15 15:28:00 +0700

    Four

---
+++ 4/program.js
@@ -0,0 +1,11 @@
+
+const { Readable } = require('stream')
+
+class ReadableStream extends Readable {
+  _read (size) {
+  }
+}
+
+const stream = new ReadableStream()
+stream.push(process.argv[2])
+stream.pipe(process.stdout)
